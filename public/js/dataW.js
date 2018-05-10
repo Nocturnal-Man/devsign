58bb9e1ccdda039ecfefa73db3246bb2087494b3
unknown <dktjs682@gmail.com>
2018-05-10 18:42:21 +0900

    git add

--- public/js/dataW.js
+++ public/js/dataW.js
@@ -13,65 +13,71 @@ console.log("data 쓰기 js 종료");
 console.log("변경 js 실행");
 $("#BTN_SIGNUP").click(function () {
     console.log("변경버튼누름");
+    firebase.database().ref('USER/ID').on('value', function (snapshot) {
+        var new_ID = $("#ID").val();
+        var updates1 = {};
+        var ID_config = snapshot.val();
+        if (ID_config = "NEW") {
+            updates1['USER/ID'] = new_ID;
+            firebase.database().ref().update(updates1);
+            console.log("ID:%s 로 메시지변경", new_ID);
+        } else {
 
-    var new_ID = $("#ID").val();
-    var updates1 = {};
-    firebase.database().ref('USER/ID').on('value',function(snapshot){
-      var ID_config = snapshot.val();
-      if(ID_config == "NEW"){}
-      else {
-        updates1['USER/ID'] = new_ID;
-        firebase.database().ref().update(updates1);
-        console.log("ID:%s 로 메시지변경", new_ID);
-      }
+        }
     });
 
-    var new_PASSWORD = $("#PASSWORD").val();
-    var updates2 = {};
-    firebase.database().ref('PASSWORD/PASSWORD').on('value',function(snapshot){
-      var PASSWORD_config = snapshot.val();
-      if(PASSWORD_config == "NEW"){}
-      else {
-        updates2['USER/PASSWORD'] = new_PASSWORD;
-        firebase.database().ref().update(updates2);
-        console.log("password:%s 로 메시지변경", new_PASSWORD);
-      }
+    firebase.database().ref('PASSWORD/PASSWORD').on('value', function (snapshot) {
+        var new_PASSWORD = $("#PASSWORD").val();
+        var updates2 = {};
+        var PASSWORD_config = snapshot.val();
+        if (PASSWORD_config == "NEW") {
+            updates2['USER/PASSWORD'] = new_PASSWORD;
+            firebase.database().ref().update(updates2);
+            console.log("password:%s 로 메시지변경", new_PASSWORD);
+        } else {
+            
+        }
     });
 
-    var new_FIRSTNAME = $("#Name").val();
-    var updates3 = {};
-    firebase.database().ref('USER/NAME').on('value',function(snapshot){
-      var NAME_config = snapshot.val();
-      if(NAME_config == "NEW"){}
-      else{
-        updates3['USER/NAME'] = new_FIRSTNAME;
-        firebase.database().ref().update(updates3);
-        console.log("Name:%s 로 메시지변경", new_FIRSTNAME);
-      }
+
+    firebase.database().ref('USER/NAME').on('value', function (snapshot) {
+        var new_FIRSTNAME = $("#Name").val();
+        var updates3 = {};
+        var NAME_config = snapshot.val();
+        if (NAME_config == "NEW") {
+            updates3['USER/NAME'] = new_FIRSTNAME;
+            firebase.database().ref().update(updates3);
+            console.log("Name:%s 로 메시지변경", new_FIRSTNAME);
+        } else {
+
+        }
     });
 
-    var new_PHONENUM = $("#Phone_num").val();
-    var updates4 = {};
-    firebase.database().ref('USER/PHONENUM').on('value',function(snapshot){
-      var PHONE_config = snapshot.val();
-      if(PHONE_config == "NEW"){}
-      else{
-        updates5['USER/PHONENUM'] = new_PASSWORD;
-        firebase.database().ref().update(updates4);
-        console.log("phone:%s 로 메시지변경", new_PHONENUM);
-      }
+
+    firebase.database().ref('USER/PHONENUM').on('value', function (snapshot) {
+        var new_PHONENUM = $("#Phone_num").val();
+        var updates4 = {};
+        var PHONE_config = snapshot.val();
+        if (PHONE_config == "NEW") {
+            updates4['USER/PHONENUM'] = new_PASSWORD;
+            firebase.database().ref().update(updates4);
+            console.log("phone:%s 로 메시지변경", new_PHONENUM);
+        } else {
+
+        }
     });
 
-    var new_BIRTHDAY = $("#Birth_Month").val();
-    var updates5 = {};
-    firebase.database().ref('USER/BIRTHDAY').on('value',function(snapshot){
-      var BIRTHDAY_config = snapshot.val();
-      if(BIRTHDAY_config == "NEW"){}
-      else{
-        updates5['USER/BIRTHDAY'] = new_PASSWORD;
-        firebase.database().ref().update(updates5);
-        console.log("birthday:%s 로 메시지변경", new_BIRTHDAY);
-      }
+    firebase.database().ref('USER/BIRTHDAY').on('value', function (snapshot) {
+        var new_BIRTHDAY = $("#Birth_Month").val();
+        var updates5 = {};
+        var BIRTHDAY_config = snapshot.val();
+        if (BIRTHDAY_config == "NEW") {
+            updates5['USER/BIRTHDAY'] = new_PASSWORD;
+            firebase.database().ref().update(updates5);
+            console.log("birthday:%s 로 메시지변경", new_BIRTHDAY);
+        } else {
+
+        }
     });
     console.log("변경 js 종료");
 });
