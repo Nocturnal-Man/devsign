a985ce1326cee46b184a145940b84c5b64e31bea
unknown <dktjs682@gmail.com>
2018-05-10 16:58:09 +0900

    Initial Commit

---
+++ public/js/dataW.js
@@ -0,0 +1,74 @@
+console.log("data 쓰기 js 실행");
+
+firebase.database().ref('USER/ID').set('NEW');
+firebase.database().ref('USER/PASSWORD').set('NEW');
+firebase.database().ref('USER/NAME').set('NEW');
+firebase.database().ref('USER/PHONENUM').set('NEW');
+firebase.database().ref('USER/SIGNDATE').set('NEW');
+
+console.log("data 쓰기 js 종료");
+
+
+
+console.log("변경 js 실행");
+$("#BTN_SIGNUP").click(function () {
+    console.log("변경버튼누름");
+
+    var new_ID = $("#ID").val();
+    var updates1 = {};
+    if (new_ID == NULL) {
+        alert("아이디가 입력되지 않았습니다.");
+        console.log("ID가 입력되지 않음", new_ID);
+    } else {
+        updates1['USER/ID'] = new_ID;
+        firebase.database().ref().update(updates1);
+        console.log("ID:%s 로 메시지변경", new_ID);
+    }
+
+    var new_PASSWORD = $("#PASSWORD").val();
+    var updates2 = {};
+    if (new_PASSWORD == NULL) {
+        alert("패스워드가 입력되지 않았습니다.");
+        console.log("PASSWORD가 입력되지 않음", new_ID);
+    } else {
+        updates2['USER/PASSWORD'] = new_PASSWORD;
+        firebase.database().ref().update(updates2);
+        console.log("password:%s 로 메시지변경", new_PASSWORD);
+    }
+
+
+    var new_FIRSTNAME = $("#Name").val();
+    var updates3 = {};
+    if (new_FIRSTNAME == NULL) {
+        alert("이름이 입력되지 않았습니다.");
+        console.log("ID가 입력되지 않음", new_ID);
+    } else {
+        updates3['USER/NAME'] = new_FIRSTNAME;
+        firebase.database().ref().update(updates3);
+        console.log("Name:%s 로 메시지변경", new_FIRSTNAME);
+    }
+
+
+    var new_PHONENUM = $("#Phone_num").val();
+    var updates4 = {};
+    if (new_PHONENUM == NULL) {
+        alert("전화번호가 입력되지 않았습니다.");
+        console.log("ID가 입력되지 않음", new_ID);
+    } else {
+        updates5['USER/PHONENUM'] = new_PASSWORD;
+        firebase.database().ref().update(updates4);
+        console.log("phone:%s 로 메시지변경", new_PHONENUM);
+    }
+    
+    var new_BIRTHDAY = $("#Birth_Month").val();
+    var updates5 = {};
+    if (new_PHONENUM == NULL) {
+        alert("전화번호가 입력되지 않았습니다.");
+        console.log("ID가 입력되지 않음", new_ID);
+    } else {
+        updates5['USER/BIRTHDAY'] = new_PASSWORD;
+        firebase.database().ref().update(updates5);
+        console.log("birthday:%s 로 메시지변경", new_BIRTHDAY);
+    }
+    console.log("변경 js 종료");
+});
---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCB-UAilnsfNeHFcPZTAGwVK5dxDZ5n6F0",
+      authDomain: "devsign-a7066.firebaseapp.com",
+      databaseURL: "https://devsign-a7066.firebaseio.com",
+      projectId: "devsign-a7066",
+      storageBucket: "devsign-a7066.appspot.com",
+      messagingSenderId: "1098166753532"
+};
+firebase.initializeApp(config);
+console.log("파이어베이스 정보 받아오기");
